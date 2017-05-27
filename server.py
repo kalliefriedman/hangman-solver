7241b06b53328ecd2af5ae312ae0c3725fdc4529
vagrant <vagrant@vagrant.vm>
2017-05-27 18:38:31 +0000

    fixing logic to print out game progress and exit while loop at correct time

--- server.py
+++ server.py
@@ -3,19 +3,10 @@ from sys import argv
 from pprint import pprint
 import json
 
-def start_game(email):
-    """ """
-    return response
-    # game_id = response["game_id"]
-    # word = response["gameId"]
-    # guesses_left = response["guessesLeft"]
-
 
 def get_status(game_id):
     """ """
     response = requests.get("http://int-sys.usr.space/hangman/games/" + game_id)
-    # word = response["word"]
-    # guesses_left = response["guessesLeft"]
     return response.json()
 
 def make_guess(game_id, guess):
@@ -32,15 +23,20 @@ def play_game(email):
     guesses_left = decoded_response["guessesLeft"]
     letters_to_guess = "EARIOTNSLCUDPMHGBFYWKVXZJQ"
     counter = 0
-    while guesses_remaining > 0 and WORD DOESN"T CONTAIN "_":
+    while guesses_left > 0 and "_" in word:
         decoded_guess_response = make_guess(game_id, letters_to_guess[counter])
         counter += 1
         decoded_status_response = get_status(game_id)
         word = decoded_status_response["word"]
-        guesses_left = decoded_guess_response["GuessesLeft"]
+        guesses_left = decoded_guess_response["guessesLeft"]
+        print word
 
-    # if guesses remaining then print "YOU WON, the word is ""
-    # if no guesses remining print "here's what you got so far. Good try"
+    if guesses_left:
+        print "You Won!"
+        print "The word was " + word
+    else:
+        print "Nice try, you didn't guess it. Here's how far you got."
+        print "The word was " + word
 
 email = raw_input("Enter your email to play: ")
 play_game(email)

be9940a7532f3b8848c99f47f4fb58020f8bc90b
vagrant <vagrant@vagrant.vm>
2017-05-29 23:11:03 +0000

    added comments

--- server.py
+++ server.py
@@ -1,32 +1,38 @@
 import requests
-from sys import argv
-from pprint import pprint
-import json
 
 
 def get_status(game_id):
-    """ """
+    """Takes in game_id as a param and returns python dictionary containing response"""
     response = requests.get("http://int-sys.usr.space/hangman/games/" + game_id)
     return response.json()
 
+
 def make_guess(game_id, guess):
-    """ """
+    """Takes in a game_id and guess as params and returns python dict containing response"""
     response = requests.post("http://int-sys.usr.space/hangman/games/"+game_id+"/guesses", data={'char': guess})
     return response.json()
 
 
 def play_game(email):
     response = requests.post('http://int-sys.usr.space/hangman/games', data={'email': email})
+    # converting response from json object into python dictionary
     decoded_response = response.json()
+
+    # setting inital variables from response
     game_id = decoded_response["gameId"]
     word = decoded_response["word"]
     guesses_left = decoded_response["guessesLeft"]
+
+    # letters in alphabet in order from most to least frequently occuring
     letters_to_guess = "EARIOTNSLCUDPMHGBFYWKVXZJQ"
+
+    # creating counter to loop through letters_to_guess string by index
     counter = 0
     msg = ""
+
+    # guessing and resetting variables with response from API
     while guesses_left > 0 and msg is not "Congrats! You have solved this hangman!":
         decoded_guess_response = make_guess(game_id, letters_to_guess[counter])
-        print decoded_guess_response
         msg = decoded_guess_response["msg"]
         counter += 1
         guesses_left = decoded_guess_response["guessesLeft"]
@@ -39,5 +45,6 @@ def play_game(email):
     else:
         print msg
 
+# starting game
 email = raw_input("Enter your email to play: ")
 play_game(email)
